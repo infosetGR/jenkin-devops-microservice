d0b40c3a2ed28086fe9b55eb1304e9c7d93606e4
Fotis Stathopoulos <fotisss@gmail.com>
2020-06-23 23:59:04 +0300

    first commit

---
+++ src/main/java/infosergr/currencyexchangeservice/HelloWorld.java
@@ -0,0 +1,11 @@
+package infosetgr.currencyexchangeservice;
+
+public class HelloWorld {
+
+    public String sayHello(String name) {
+
+        System.out.println("sayHello called with name : " + name);
+        return "Hello " + name;
+    }
+
+}
